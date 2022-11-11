6ae109fef7edbad2eba2d09c6065c282e636ad58
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2022-11-11 15:55:26 +0100

    task 2 almost done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+'use strict';
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = 0;
+
+player.on(
+  'timeupdate',
+  _.throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log(data.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    console.log(seconds);
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
