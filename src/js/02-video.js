876e2ea20d567bdd7a1ab837e5c86987e53accc2
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2022-11-11 16:01:03 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import  Player  from '@vimeo/player';
+import  throttle  from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
